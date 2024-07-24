c50d5807cc56b3dfe7c1bbd69fe3bc8f3edf5025
TruongVanThuong <tvt230101@gmail.com>
2024-07-24 21:59:18 +0700

    vercel.json 2

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
-const express = require('express');
+// const express = require('express');
 // const fs = require('fs').promises;
 // const path = require('path');
 // const cors = require('cors');
 
-const app = express();
-const PORT = 5000;
+// const app = express();
+// const PORT = 5000;
 // const DATA_FILE = path.join(__dirname, 'ordersStatusData.json');
 
-app.use("/",(req, res)=> {
-  res.send("runing server");
-});
 // app.use(cors());
 // app.use(express.json());
 
@@ -95,7 +92,20 @@ app.use("/",(req, res)=> {
 //   }
 // });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-  const hello = "hello";
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app;
